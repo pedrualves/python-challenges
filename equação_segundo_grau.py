33c3b99e708a159879315408d59035e53404217e
Pedro <contato@pedrualves.com>
2021-02-04 21:17:08 -0300

    Create equação_segundo_grau.py

---
+++ equação_segundo_grau.py
@@ -0,0 +1,35 @@
+import math
+from fractions import Fraction
+
+
+def resolve_segundo_grau(a: int, b: int, c: int):
+    equacao_incompleta = False
+
+    if b == 0 or c == 0:
+        equacao_incompleta = True
+
+    delta = b * b - (4 * a * c)
+
+    if delta < 0:
+        if equacao_incompleta:
+            print('equação incompleta, com raiz com número negativo, logo conjunto solução é 0')
+        else:
+            print('delta negativo, não admite solução')
+        return
+
+    raiz = math.sqrt(delta)
+
+    x1 = ((-1 * b) + raiz) / (2 * a)
+    x2 = ((-1 * b) - raiz) / (2 * a)
+    if not isinstance(x1, int):
+        x1 = Fraction(x1).limit_denominator(10)
+    if not isinstance(x2, int):
+        x2 = Fraction(x2).limit_denominator(10)
+    print(x1, x2)
+
+
+resolve_segundo_grau(3, -1, -2)  # 1 e 2/3
+resolve_segundo_grau(2, -7, 0)  # 7/2 0
+resolve_segundo_grau(4, 0, 2)  # equação incompleta, com raiz com número negativo, logo conjunto solução é 0
+resolve_segundo_grau(4, 0, -16)  # 2 e -2
+resolve_segundo_grau(5, 8, 10)  # delta negativo, não admite solução
