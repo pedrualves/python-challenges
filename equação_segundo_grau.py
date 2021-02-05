223673112783ae576014ab61b97a9521812234c0
Pedro <contato@pedrualves.com>
2021-02-04 21:27:37 -0300

    Update equação_segundo_grau.py

--- equação_segundo_grau.py
+++ equação_segundo_grau.py
@@ -20,16 +20,19 @@ def resolve_segundo_grau(a: int, b: int, c: int):
     raiz = math.sqrt(delta)
 
     x1 = ((-1 * b) + raiz) / (2 * a)
-    x2 = ((-1 * b) - raiz) / (2 * a)
     if not isinstance(x1, int):
         x1 = Fraction(x1).limit_denominator(10)
+    
+    x2 = ((-1 * b) - raiz) / (2 * a)
     if not isinstance(x2, int):
         x2 = Fraction(x2).limit_denominator(10)
+        
     print(x1, x2)
+    return
 
 
-resolve_segundo_grau(3, -1, -2)  # 1 e 2/3
-resolve_segundo_grau(2, -7, 0)  # 7/2 0
-resolve_segundo_grau(4, 0, 2)  # equação incompleta, com raiz com número negativo, logo conjunto solução é 0
-resolve_segundo_grau(4, 0, -16)  # 2 e -2
-resolve_segundo_grau(5, 8, 10)  # delta negativo, não admite solução
+resolve_segundo_grau(3, -1, -2)  # resultado: 1 e 2/3
+resolve_segundo_grau(2, -7, 0)  # resultado: 7/2 0
+resolve_segundo_grau(4, 0, 2)  # resultado: equação incompleta, com raiz com número negativo, logo conjunto solução é 0
+resolve_segundo_grau(4, 0, -16)  # resultado: 2 e -2
+resolve_segundo_grau(5, 8, 10)  # resultado: delta negativo, não admite solução
