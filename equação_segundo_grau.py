4a1861fbc1676b954a02c7a9ce54dc2b587b3e6e
Pedro <contato@pedrualves.com>
2021-02-25 13:55:34 -0300

    Update equação_segundo_grau.py

--- equação_segundo_grau.py
+++ equação_segundo_grau.py
@@ -3,31 +3,26 @@ from fractions import Fraction
 
 
 def resolve_segundo_grau(a: int, b: int, c: int):
-    equacao_incompleta = False
-
-    if b == 0 or c == 0:
-        equacao_incompleta = True
-
     delta = b * b - (4 * a * c)
 
-    if delta < 0:
-        if equacao_incompleta:
-            print('equação incompleta, com raiz com número negativo, logo conjunto solução é 0')
-        else:
-            print('delta negativo, não admite solução')
-        return
+    if delta < 0 and (b == 0 or c == 0):
+        print('equação incompleta, com raiz com número negativo, logo conjunto solução é 0')
+    else:
+        print('delta negativo, não admite solução')
 
-    raiz = sqrt(delta)
+    if delta > 0:
+        raiz = sqrt(delta)
 
-    x1 = ((-1 * b) + raiz) / (2 * a)
-    if not isinstance(x1, int):
-        x1 = Fraction(x1).limit_denominator(10)
+        x1 = ((-1 * b) + raiz) / (2 * a)
+        if not isinstance(x1, int):
+            x1 = Fraction(x1).limit_denominator(10)
 
-    x2 = ((-1 * b) - raiz) / (2 * a)
-    if not isinstance(x2, int):
-        x2 = Fraction(x2).limit_denominator(10)
+        x2 = ((-1 * b) - raiz) / (2 * a)
+        if not isinstance(x2, int):
+            x2 = Fraction(x2).limit_denominator(10)
 
-    print(x1, x2)
+        print(x1, x2)
+    
     return
 
 
